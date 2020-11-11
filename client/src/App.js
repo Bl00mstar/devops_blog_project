18d32a1d47aa613797d5e16ee7031fffa2179fd6
Bl00mstar <54003799+Bl00mstar@users.noreply.github.com>
2020-11-12 00:47:29 +0100

    setup frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import "./App.css";
+import React, { useEffect } from "react";
+// import { connect } from "react-redux";
+// import { useNavigate } from "react-router-dom";
 
-function App() {
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+import { useDispatch } from "react-redux";
+
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>asd</div>
     </div>
   );
-}
+};
 
 export default App;
