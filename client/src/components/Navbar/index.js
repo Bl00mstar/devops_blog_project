04fba2cfa04c26e01a0e03e796cf28cdbefdbb40
Bl00mstar <54003799+Bl00mstar@users.noreply.github.com>
2020-11-12 02:22:53 +0100

    little styling

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -43,66 +43,73 @@ export default function Navbar() {
   let element = useRoutes(routes);
   return (
     <div>
-      <nav className='nav-extended N/A transparent z-depth-0'>
-        <div className='nav-wrapper container'>
-          <a href='#!' data-target='mobile-demo' className='sidenav-trigger'>
-            <i className='material-icons' style={{ color: "#039be5 " }}>
-              menu
-            </i>
-          </a>
+      <header>
+        <nav className='nav-extended N/A transparent z-depth-0'>
+          <div className='nav-wrapper container'>
+            <a href='#!' data-target='mobile-demo' className='sidenav-trigger'>
+              <i className='material-icons' style={{ color: "#039be5 " }}>
+                menu
+              </i>
+            </a>
+            <ul
+              id='nav-mobile'
+              class='sidenav sidenav-fixed'
+              style={{ transform: "translateX(0%)" }}
+            >
+              <li>
+                <NavLink
+                  activeClassName='nav-link blue-text '
+                  inactiveClassName='nav-link grey-text grey-darken-2'
+                  className='nav-link '
+                  to='/'
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  activeClassName='nav-link blue-text '
+                  inactiveClassName='nav-link grey-text grey-darken-2'
+                  className='nav-link '
+                  to='/about'
+                >
+                  About
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
-          <ul className='right hide-on-med-and-down nav-link'>
-            <li>
-              <NavLink
-                activeClassName='nav-link blue-text '
-                inactiveClassName='nav-link grey-text grey-darken-2'
-                className='nav-link '
-                to='/'
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                activeClassName='nav-link blue-text '
-                inactiveClassName='nav-link grey-text grey-darken-2'
-                className='nav-link '
-                to='/about'
-              >
-                About
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <ul
-        className='sidenav sidenav-close transparent z-depth-0'
-        id='mobile-demo'
-        style={{ top: "50px", width: "150px" }}
-      >
-        <li>
-          <NavLink
-            activeClassName='nav-link blue-text '
-            inactiveClassName='nav-link grey-text grey-darken-2'
-            className='nav-link '
-            to='/'
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            activeClassName='nav-link blue-text '
-            inactiveClassName='nav-link grey-text grey-darken-2'
-            className='nav-link '
-            to='/about'
-          >
-            About
-          </NavLink>
-        </li>
-      </ul>
-      {element}
+        <ul
+          className='sidenav sidenav-close transparent z-depth-0'
+          id='mobile-demo'
+          style={{ top: "50px", width: "150px" }}
+        >
+          <li>
+            <NavLink
+              activeClassName='nav-link blue-text '
+              inactiveClassName='nav-link white-text '
+              className='nav-link '
+              to='/'
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName='nav-link blue-text '
+              inactiveClassName='nav-link white-text '
+              className='nav-link '
+              to='/about'
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </header>
+      <main>
+        <div className='container'>{element}</div>
+      </main>
     </div>
   );
 }
